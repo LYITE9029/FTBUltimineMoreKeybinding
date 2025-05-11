e3ed03213328311cd889c3776bf8901d62f8e7a9
LYITE9029 <98957990+LYITE9029@users.noreply.github.com>
2025-05-12 08:12:04 +0900

    Delete lyite9029/ftbultimine_more_keybinding/mixin directory

--- lyite9029/ftbultimine_more_keybinding/mixin/MixinFTBUltimine.java
+++
@@ -1,8 +0,0 @@
-package lyite9029.ftbultimine_more_keybinding.mixin;
-
-import dev.ftb.mods.ftbultimine.FTBUltimine;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(FTBUltimine.class)
-public class MixinFTBUltimine {
-}
