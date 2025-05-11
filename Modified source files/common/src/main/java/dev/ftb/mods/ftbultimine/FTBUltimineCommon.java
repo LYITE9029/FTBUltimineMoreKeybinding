447c0a8841566f875bf4fb65f21a0a141d64ca4b
LYITE9029 <98957990+LYITE9029@users.noreply.github.com>
2025-05-12 08:05:18 +0900

    Create FTBUltimineCommon.java

---
+++ Modified source files/common/src/main/java/dev/ftb/mods/ftbultimine/FTBUltimineCommon.java
@@ -0,0 +1,21 @@
+package dev.ftb.mods.ftbultimine;
+
+import net.minecraft.core.BlockPos;
+
+import java.util.List;
+
+/**
+ * @author LatvianModder
+ */
+public class FTBUltimineCommon {
+	public void setShape(int shapeIdx, List<BlockPos> blocks) {
+	}
+
+    public void editConfig(boolean isClientConfig) {
+    }
+
+    /*========== here ↓==========*/
+    public void setCanUseSetPacket(boolean canUseSetPacket) {
+    }
+    /*========== here ↑==========*/
+}
